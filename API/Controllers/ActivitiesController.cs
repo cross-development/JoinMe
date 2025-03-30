a8606dfa3a41650492613fddd7a3db3edc84d2f4
Vitaliy Derda <derdavitaliy@gmail.com>
2025-03-30 10:50:11 +0300

    feat: added a walking skeleton for api

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Domain;
+
+namespace API.Controllers;
+
+public class ActivitiesController(ApplicationDbContext dbContext) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await dbContext.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await dbContext.Activities.FindAsync(id);
+
+        if (activity is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(activity);
+    }
+}
---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
