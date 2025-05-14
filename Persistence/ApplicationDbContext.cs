113a15537eb67bbdd6f605c81b6b94e1741f5758
Vitalii Derda <derdavitaliy@gmail.com>
2025-05-14 20:21:24 +0300

    feat: configured activity attendee relationship

--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -7,4 +7,23 @@ namespace Persistence;
 public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Activity> Activities { get; set; }
+    public required DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ActivityAttendee>(entityBuilder => entityBuilder
+            .HasKey(activityAttendee => new { activityAttendee.ActivityId, activityAttendee.UserId }));
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(activityAttendee => activityAttendee.User)
+            .WithMany(user => user.Activities)
+            .HasForeignKey(activityAttendee => activityAttendee.UserId);
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(activityAttendee => activityAttendee.Activity)
+            .WithMany(activity => activity.Attendees)
+            .HasForeignKey(activityAttendee => activityAttendee.ActivityId);
+    }
 }
